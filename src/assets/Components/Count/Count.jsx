6713a9698e4a7ee2a99ca226a22a802187091038
saif347 <skhwtsaif@gmail.com>
2024-02-17 19:26:10 +0600

    counter increment and decrement functionality added

--- src/assets/Components/Count/Count.jsx
+++ src/assets/Components/Count/Count.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
 
 
 const Count = () => {
+    const [count, setCount] = useState(1);
+    const handleSum = ()=>{
+        setCount(count + 1);
+    }
+
+    const handleDecrement = ()=>{
+        if(count === 1){
+             return;
+        }
+        setCount(count - 1)
+    }
     return (
         <div className='border flex items-center justify-between w-28 '>
-            <button className='border-r py-2 px-3 bg-gray-400'>-</button>
-            <span className='px-3'>1</span>
-            <button className='border-r py-2 px-3 bg-gray-400'>+</button>
+            <button onClick={handleDecrement} className='border-r py-2 px-3 bg-gray-400'>-</button>
+            <span id="count" className='px-3'>{count}</span>
+            <button onClick={handleSum} className='border-r py-2 px-3 bg-gray-400'>+</button>
         </div>
     );
 };
